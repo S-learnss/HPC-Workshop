09f1f77dcc4aad2b20bd45c5452884df9bb01715
S-learnss <s23mlomzale@gmail.com>
2026-04-08 11:22:22 +0200

    Complete task 1: code.c and Makefile

--- tutorial1/app/code.c
+++ tutorial1/app/code.c
@@ -1,16 +1,21 @@
 // INCLUDES
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
 #include "code.h"
 
 // GLOBALS
 
 
-//FUNCTIONS
+// FUNCTIONS
+
 // welcome message
 void welcome_message()
 {
     printf("Welcome!\n");
 }
+
 // get group name
 void get_name(char* name)
 {
@@ -21,11 +26,24 @@ void get_name(char* name)
 // 1. Function to create a folder using the name variable as its name
 void create_folder(char* name)
 {
-
+    mkdir(name, 0777);
 }
 
-// 2. Function to create a file in the folder <folder_name> called group.txt where you insert the <group_name> as text
+// 2. Function to create a file in the folder <folder_name>
+// called group.txt where you insert the <group_name> as text
 void create_file(char* folder_name, char* group_name)
 {
+    char path[256];
 
+    // Create full file path
+    sprintf(path, "%s/group.txt", folder_name);
+
+    FILE *fp = fopen(path, "w");
+
+    if (fp != NULL)
+    {
+        fprintf(fp, "%s", group_name);
+        fclose(fp);
+    }
 }
+
